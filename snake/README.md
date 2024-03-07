2956e08f44bc02e244f547e2714ea71297ca080c
Shraddha Sharma <shr.comp1@gmail.com>
2024-03-07 17:24:36 -0500

    Add files via upload

---
+++ snake/README.md
@@ -0,0 +1,2 @@
+# Snake-Game-cpp
+# Snake-Game-cpp
---
+++ snake/snake_game.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>  // for generating random numbers (ex. food for snake ) and for arrays and stuff
+#include <conio.h>  // generating triggers on keyboard hits with keyboard hit library (kbhit func)
+#include <windows.h>  // drawing characters at different coordinates on console, not only (0,0)(default)
+
+using namespace std;
+
+#define MAX_LENGTH 1000
+
+// Directions (read-only global vars)
+
+const char DIR_UP = 'U';
+const char DIR_DOWN = 'D';
+const char DIR_LEFT = 'L';
+const char DIR_RIGHT = 'R';
+
+// global console window size vars
+
+int consoleWidth , consoleHeight;
+
+int main()
+{
+25
+
+
+return 0;
+}
